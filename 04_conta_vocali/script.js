12d4774a9674bc4939e95f03c62b582f7debba5c
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-24 16:04:05 +0100

    [UPD] done exercise 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function listaVocali(string) {
+    const vocali = ['a', 'e', 'i', 'o', 'u'];
+    let risultato = 0;
 
-
+    for (let i = 0; i < string.length; i++){
+        if(vocali.includes(string[i].toLowerCase())){
+            risultato++;
+        }
+    }
+    return risultato;
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+const numeroVocali = listaVocali(word);
+console.log(numeroVocali); 
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
