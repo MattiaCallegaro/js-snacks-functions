ec00f82cc8ea89778283e63465782dec6cbb2e9c
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-24 15:17:55 +0100

    [UPD] done exercise 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function createNewArray(parola){
+    let nuovoArray= [];
+    for(let i=0; i < parola.length; i++ ){
+       nuovoArray.push(parola[i].charAt(0));
+    }
 
+    return nuovoArray;
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+createNewArray(names);
+console.log(createNewArray(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
